9d18b4b5605adb1e2139b9f5319da08a4f3f7653
Anna Ho <aymieoh@gmail.com>
2026-01-23 11:43:11 -0500

    transitioning to redux for application state management... time to start exploring resolutions for my prop drilling issue...

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,13 +2,16 @@ import "./bootstrap";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+// import store from "./store"
 import { DataContext } from "./contexts/DataContext";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(
     // <React.StrictMode>
-    <DataContext>
-        <App />
-    </DataContext>
+    // <Provider store={store}> 
+        <DataContext>
+            <App />
+        </DataContext>
+    // </Provider>
     // </React.StrictMode>
 );
---
+++ resources/js/store/slices/addressSlice.js
@@ -0,0 +1,2 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { AddressService } from "../../services/addressService";
